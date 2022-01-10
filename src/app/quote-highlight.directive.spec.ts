fc5df0fd911836a67bfc740fd95b27e4344a9da7
annngure <annwanjikungure@gmail.com>
2022-01-11 01:40:39 +0300

    add components

---
+++ src/app/quote-highlight.directive.spec.ts
@@ -0,0 +1,7 @@
+import { QuoteHighlight.Directive } from './quote-highlight.directive';
+
+describe('QuoteHighlight.Directive', () => {
+  it('should create an instance', () => {
+    expect(new QuoteHighlight.Directive()).toBeTruthy();
+  });
+});
